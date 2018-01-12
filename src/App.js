cc83f5805f3f49346e42c8c568ee2993ee0a9827
Tom Ritland <t_ritland@yahoo.com>
2018-01-11 20:52:12 -0600

    created a basic component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello World, I'm a React App </h1> 
+        <p>This is working</p>
+        <Person />
       </div>
+      
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
   }
 }
 
