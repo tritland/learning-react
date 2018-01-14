ee16b634c1e3e3b26ee9efbdd36fc1c558ec68b8
Tom Ritland <t_ritland@yahoo.com>
2018-01-13 21:10:13 -0600

    deleting person with index and splice

--- src/App.js
+++ src/App.js
@@ -13,18 +13,6 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('button clicked');
-    // DON'T DO THIS: this.state.persons[0].name = "Thomas";
-    this.setState({
-      persons: [
-        { name: newName, age: 33 },
-        { name: "Chels", age: 32 },
-        { name: "Desmond", age: 50 }
-      ]
-    })
-  }
-
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
@@ -34,6 +22,12 @@ class App extends Component {
       ]
     })
   }
+
+deletePersonHandler = (personIndex) => {
+  const persons = this.state.persons;
+  persons.splice(personIndex, 1);
+  this.setState({persons: persons});
+}
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
@@ -54,8 +48,8 @@ let persons = null;
 if (this.state.showPersons) {
   persons = (
     <div>
-      {this.state.persons.map(person => {
-          return <Person name={person.name} age={person.age} />
+      {this.state.persons.map((person, index) => {
+          return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} />
       })}
   </div> 
   );
