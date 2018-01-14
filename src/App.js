381ff76149ddc8c67e665067f115a4983289b9bc
Tom Ritland <t_ritland@yahoo.com>
2018-01-13 22:16:31 -0600

    updated nameChangedHandler

--- src/App.js
+++ src/App.js
@@ -5,33 +5,43 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      { name: "Tom", age: 33 },
-      { name: "Chels", age: 32 },
-      { name: "Desmond", age: 6 }
+      { id: 1, name: "Tom", age: 33 },
+      { id: 2, name: "Chels", age: 32 },
+      { id: 3, name: "Desmond", age: 6 }
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Tom', age: 33 },
-        { name: event.target.value, age: 32 },
-        { name: "Desmond", age: 50 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons })
   }
 
-deletePersonHandler = (personIndex) => {
-  const persons = this.state.persons;
-  persons.splice(personIndex, 1);
-  this.setState({persons: persons});
-}
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.slice(); or use the more moder ES6 spread operator below
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState({ showPersons: !doesShow });
   }
 
   render() {
@@ -43,24 +53,24 @@ deletePersonHandler = (personIndex) => {
       cursor: 'pointer'
     }
 
-let persons = null;
+    let persons = null;
 
-if (this.state.showPersons) {
-  persons = (
-    <div>
-      {this.state.persons.map((person, index) => {
-          return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} />
-      })}
-  </div> 
-  );
-}
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} key={person.id} changed={(event) => this.nameChangedHandler(event, person.id)} />
+          })}
+        </div>
+      );
+    }
 
     return (
       <div className="App">
         <h1>Hello World, I'm a React App </h1>
         <p>This is working</p>
-        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button> 
-        {persons}       
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
 
     );
