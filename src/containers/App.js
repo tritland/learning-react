5cbd67f150b12c5704e003262cd974b39f53ae65
Tom Ritland <t_ritland@yahoo.com>
2018-02-15 11:53:49 -0600

    lifecycle hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,35 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
+  constructor(props) {
+    super(props)
+    console.log('[App.js] inside constructor', props);
+  }
+
+  componentWillMount(){
+    console.log('[App.js] Inside componentWillMount()')
+  }
+
+componentDidMount(){
+  console.log('[App.js] Inside componentDidMount()')
+}
+
+// shouldComponentUpdate(nextProps, nextState){
+//   console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+//   return nextState.persons !== this.state.persons || nextState.showPersons !== this.state.showPersons;
+// }
+
+componentWillUpdate(nextProps, nextState){
+  console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
+}
+
+componentDidUpdate(){
+  console.log('[UPDATE App.js] Inside componentDidUpdate');
+}
+
   state = {
     persons: [
       { id: 1, name: "Tom", age: 33 },
@@ -43,6 +69,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] inside render()')
     let persons = null;
 
     if (this.state.showPersons) {
@@ -58,6 +85,7 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
+      <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           showPersons={this.state.showPersons}
           persons={this.state.persons}
